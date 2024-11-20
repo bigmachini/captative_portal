51d3941437dd1f8ac9de96970dd848a3e9c44093
bigmachini <elisha.wambiji@bigmachini.net>
2024-11-20 12:07:28 +0000

    refactor code

--- app.py
+++ app.py
@@ -16,35 +16,6 @@ REDIRECT_URL = os.getenv('REDIRECT_URL')
 BUSINESS_NAME = os.getenv('BUSINESS_NAME')
 
 
-# SESSION_UUID = str(uuid.uuid4())
-
-
-@app.route('/test')
-def test():
-    mac = '00:0c:29:8e:6d:6c'
-    ip = '192.168.11.20'
-    link_login = '192.168.11.11'
-    link_login_only = '192.168.3.3'
-
-    # Prepare the data to be sent in the POST request
-    data = {
-        'mac': mac,
-        'ip': ip,
-        'link-login': link_login,
-        'link-login-only': link_login_only,
-        'error': 'error'
-    }
-
-    # Make a POST request to the index route
-    response = requests.post(url_for('index', _external=True), data=data)
-
-    # Handle the response if needed
-    if response.status_code == 200:
-        return response.text
-    else:
-        return "Failed to redirect to index", response.status_code
-
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
@@ -53,7 +24,7 @@ def index():
             'ip': request.form['ip'],
             'link_login': request.form['link-login'],
             'link_login_only': request.form['link-login-only'],
-            'error': request.form['error']
+            'error': request.form['error'],
         }
 
         api_url = f"{BASE_API_URL}/api/user/{app_data['mac']}"
@@ -61,11 +32,7 @@ def index():
         headers = {'Content-Type': 'application/json'}
         response = requests.get(api_url, json=data, headers=headers)
         if response.status_code == 200:
-            return render_template('connect.html',
-                                   business_name=BUSINESS_NAME,
-                                   link_login_only=app_data['link_login_only'],
-                                   linkorig=REDIRECT_URL,
-                                   app_data=app_data)
+            return redirect(url_for('connect', **app_data))
 
         api_url = f"{BASE_API_URL}/api/user/packages/{PARTNER_ID}"
         response = requests.get(api_url)
@@ -76,6 +43,21 @@ def index():
                                app_data=app_data)
 
     return render_template('marketting.html')
+
+
+@app.route('/connect')
+def connect():
+    app_data = {
+        'mac': request.args.get('mac'),
+        'ip': request.args.get('ip'),
+        'link_login': request.args.get('link-login'),
+        'link_login_only': request.args.get('link-login-only'),
+        'error': request.args.get('error')
+    }
+    return render_template('connect.html',
+                           business_name=BUSINESS_NAME,
+                           linkorig=REDIRECT_URL,
+                           app_data=app_data)
 
 
 @app.route('/subscribe', methods=['POST'])
@@ -101,11 +83,10 @@ def subscribe():
     }
     headers = {'Content-Type': 'application/json'}
     response = requests.post(api_url, json=data, headers=headers)
-
+    print(f"subscribe response: {response}")
     if response.status_code == 200:
-        return render_template('connect.html',
-                               business_name=BUSINESS_NAME,
-                               app_data=app_data)
+        # Redirect to the connect route with query parameters
+        return redirect(url_for('connect', **app_data))
 
     else:
         return redirect(url_for('failure'))
